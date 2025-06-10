674283e4ce545152b80936b5cbd1313b0fa91b78
Fardan-Nurhidayat <fardannurhidayat07.stu@pnc.ac.id>
2025-06-11 03:00:42 +0700

    refactor any kde for deplyment

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbmAu7wP2PmpmlH3q4txMIOfRVau4vUfM",
-  authDomain: "fincoach-3d293.firebaseapp.com",
-  projectId: "fincoach-3d293",
-  storageBucket: "fincoach-3d293.firebasestorage.app",
-  messagingSenderId: "901394584358",
-  appId: "1:901394584358:web:b4fd20cc72c7d1b53e67e7",
-  measurementId: "G-8LR4WN31LT"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
 };
 
 const app = initializeApp(firebaseConfig);
