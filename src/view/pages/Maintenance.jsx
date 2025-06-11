516ebe60af4c4356f279c4df97f667e6855f3546
Fardan-Nurhidayat <fardannurhidayat07.stu@pnc.ac.id>
2025-06-12 03:05:39 +0700

    shore : jadwal solat

--- src/view/pages/Maintenance.jsx
+++ src/view/pages/Maintenance.jsx
@@ -1,7 +1,5 @@
 // pages/maintenance.js
 
-import { Link } from "react-router";
-
 export default function MaintenancePage() {
   return (
     <div className='min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 flex items-center justify-center px-4 sm:px-6 lg:px-8 dark:from-gray-900 dark:to-gray-800'>
