9975c01c5b182aaa6c3bd43a1102a944b7549086
Fardan-Nurhidayat <fardannurhidayat07.stu@pnc.ac.id>
2025-06-12 08:27:05 +0700

    test vercel.json

--- src/view/pages/Maintenance.jsx
+++ src/view/pages/Maintenance.jsx
@@ -1,5 +1,3 @@
-// pages/maintenance.js
-
 export default function MaintenancePage() {
   return (
     <div className='min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 flex items-center justify-center px-4 sm:px-6 lg:px-8 dark:from-gray-900 dark:to-gray-800'>
